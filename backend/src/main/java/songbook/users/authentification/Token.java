9bd8eaf4740588dfeaf9b77a12f29d031cd2b47a
Christian Seidel <github@parameter5.de>
2022-12-14 11:07:06 +0100

    restart

--- backend/src/main/java/songbook/users/authentification/Token.java
+++ backend/src/main/java/songbook/users/authentification/Token.java
@@ -2,9 +2,11 @@ package songbook.users.authentification;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Token {
 
     private String token;
