a45872cc00892329f2454c27c1508da0e2b72faa
Christian Seidel <github@parameter5.de>
2023-03-28 17:14:45 +0200

    undo

--- backend/src/main/java/songbook/SecurityConfig.java
+++ backend/src/main/java/songbook/SecurityConfig.java
@@ -45,7 +45,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers(HttpMethod.POST, "/api/user/register").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/index*", "/locales/**", "/static/**", "/*.js", "/*.json", "/*.ico","/*.png").permitAll()
                 .antMatchers(HttpMethod.GET, "/").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/songbook/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
